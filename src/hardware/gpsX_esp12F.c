ece04576b7f1056266481824bc5cb9231ac09944
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-01-21 23:25:09 +0530

    added src\hardware

---
+++ src/hardware/gpsX_esp12F.c
@@ -0,0 +1,7 @@
+/*
+This code is written for Esp12f Esp8266 board,
+Responsible for comunication with the centralized
+remote server for updating coordinates that will
+reflect on the app (IOS/Android)
+2025 @ Himjyoti
+*/
